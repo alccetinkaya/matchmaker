f2b2c67d787f2b9ef9b3bf14ab1b6de53f647a67
Ali Cuneyt CETINKAYA <ali.cetinkaya@siemens.com>
2023-02-20 21:51:10 +0300

    Added fail codes

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -2,12 +2,15 @@ import { ApiRespData } from "../models/api.model";
 
 export enum FailCode {
     VALIDATION_ERROR = "VALIDATION_ERROR",
-    OPERATION_ERROR = "OPERATION_ERROR"
+    OPERATION_ERROR = "OPERATION_ERROR",
+    SERVICE_ERROR = "SERVICE_ERROR"
 }
 
 export enum FailDetailCode {
     REQUIRED_PROPERTY = "REQUIRED_PROPERTY",
-    DATABASE_ERROR = "DATABASE_ERROR"
+    DATABASE_ERROR = "DATABASE_ERROR",
+    PROPERTY_CHECK = "PROPERTY_CHECK",
+    SERVICE_RESPONSE = "SERVICE_RESPONSE"
 }
 
 export function prepareSuccessMsg({ description = null, details = null, statusCode = 200 }): ApiRespData {
