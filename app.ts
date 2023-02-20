82ecb7227081922408601af69c080b2bc955545a
Ali Cuneyt CETINKAYA <ali.cetinkaya@siemens.com>
2023-02-20 22:06:54 +0300

    Added player and fixture server handling

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
 import { ApiRespData } from "./src/models/api.model";
+import { FixtureApiService } from "./src/services/api/fixture.api.service";
 import { GameApiService } from "./src/services/api/game.api.service";
+import { PlayerApiService } from "./src/services/api/player.api.service";
 import { PrismaDatabaseService } from "./src/services/prisma.database.service";
 import { removeEmptyKeys } from "./src/utils/util";
 
@@ -9,6 +11,8 @@ const app = express()
 app.use(express.json());
 
 const gameSvc = new GameApiService(new PrismaDatabaseService());
+const playerSvc = new PlayerApiService(new PrismaDatabaseService());
+const fixtureSvc = new FixtureApiService(new PrismaDatabaseService());
 
 async function sendResult(res: any, data: ApiRespData) {
   res.status(data.statusCode);
@@ -16,6 +20,7 @@ async function sendResult(res: any, data: ApiRespData) {
   res.send(await removeEmptyKeys(respData));
 }
 
+// ############ GAME API ############
 app.post('/game', async (req, res) => {
   sendResult(res, await gameSvc.add(req.body));
 })
@@ -26,6 +31,36 @@ app.get('/game', async (req, res) => {
 
 app.delete('/game', async (req, res) => {
   sendResult(res, await gameSvc.del(req.query.name));
+})
+
+// ############ FIXTURE API ############
+app.post('/fixture', async (req, res) => {
+  sendResult(res, await fixtureSvc.add(req.body));
+})
+
+app.get('/fixture', async (req, res) => {
+  sendResult(res, await fixtureSvc.get(req.query.id));
+})
+
+app.put('/fixture', async (req, res) => {
+  sendResult(res, await fixtureSvc.update(req.body));
+})
+
+app.delete('/fixture', async (req, res) => {
+  sendResult(res, await fixtureSvc.del(req.query.id));
+})
+
+// ############ PLAYER API ############
+app.post('/player', async (req, res) => {
+  sendResult(res, await playerSvc.add(req.body));
+})
+
+app.get('/player', async (req, res) => {
+  sendResult(res, await playerSvc.get(req.query.name));
+})
+
+app.delete('/player', async (req, res) => {
+  sendResult(res, await playerSvc.del(req.query.name));
 })
 
 app.listen(3000, () => {
