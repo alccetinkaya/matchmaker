e7ca4b004b59e07909bdb99cef49197c157e28e8
Ali Cuneyt CETINKAYA <ali.cetinkaya@siemens.com>
2023-04-17 10:37:03 +0300

    Added league API to handle fixture and match

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import { ApiRespData } from "./src/models/api.model";
 import { FixtureApiService } from "./src/services/api/fixture.api.service";
 import { GameApiService } from "./src/services/api/game.api.service";
+import { LeagueInfoApiService } from "./src/services/api/league-info.api.service";
 import { LeagueApiService } from "./src/services/api/league.api.service";
 import { PlayerApiService } from "./src/services/api/player.api.service";
 import { PrismaDatabaseService } from "./src/services/prisma.database.service";
@@ -14,6 +15,7 @@ app.use(express.json());
 const gameSvc = new GameApiService(new PrismaDatabaseService());
 const playerSvc = new PlayerApiService(new PrismaDatabaseService());
 const fixtureSvc = new FixtureApiService(new PrismaDatabaseService());
+const leagueInfoSvc = new LeagueInfoApiService(new PrismaDatabaseService());
 const leagueSvc = new LeagueApiService(new PrismaDatabaseService());
 
 async function sendResult(res: any, data: ApiRespData) {
@@ -65,21 +67,30 @@ app.delete('/player', async (req, res) => {
   sendResult(res, await playerSvc.del(req.query.name));
 })
 
-// ############ LEAGUE API ############
-app.post('/league', async (req, res) => {
-  sendResult(res, await leagueSvc.add(req.body));
+// ############ LEAGUE INFO API ############
+app.post('/league-info', async (req, res) => {
+  sendResult(res, await leagueInfoSvc.add(req.body));
 })
 
+app.get('/league-info', async (req, res) => {
+  sendResult(res, await leagueInfoSvc.get(req.query));
+})
+
+app.put('/league-info', async (req, res) => {
+  sendResult(res, await leagueInfoSvc.update(req.body));
+})
+
+app.delete('/league-info', async (req, res) => {
+  sendResult(res, await leagueInfoSvc.del(req.query.name));
+})
+
+// ############ LEAGUE API ############
 app.get('/league', async (req, res) => {
   sendResult(res, await leagueSvc.get(req.query));
 })
 
 app.put('/league', async (req, res) => {
-  sendResult(res, await leagueSvc.update(req.body));
-})
-
-app.delete('/league', async (req, res) => {
-  sendResult(res, await leagueSvc.del(req.query.name));
+  sendResult(res, await leagueSvc.update(req.query));
 })
 
 app.listen(3000, () => {
--- src/services/api/league.api.service.ts
+++ src/services/api/league.api.service.ts
@@ -1,6 +1,7 @@
 import { IDatabase } from "../../interfaces/database.interface";
 import { ApiRespData } from "../../models/api.model";
-import { LeagueInfoData } from "../../models/league.model";
+import { FixtureData } from "../../models/fixture.model";
+import { LeagueData } from "../../models/league.model";
 import { FailCode, FailDetailCode, prepareFailureMsg, prepareSuccessMsg } from "../../utils/api.util";
 
 export class LeagueApiService {
@@ -10,198 +11,225 @@ export class LeagueApiService {
         this.#dbSvc = dbSvc;
     }
 
-    validAddAndUpdateRequest(req: any): ApiRespData {
-        if (typeof req !== 'object') {
+    validUpdateRequest(query: any): ApiRespData {
+        if (Number.isNaN(query.id)) {
             return prepareFailureMsg({
                 failCode: FailCode.VALIDATION_ERROR,
-                failDescription: "The payload is invalid",
+                failDescription: "The query is invalid",
             }, {
                 detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                detailDescription: "Payload isn't an object"
-
-            });
-        }
-
-        let keys = ["name", "point"];
-        for (const key of keys) {
-            if (!req.hasOwnProperty(key)) {
-                return prepareFailureMsg({
-                    failCode: FailCode.VALIDATION_ERROR,
-                    failDescription: "The payload is invalid",
-                }, {
-                    detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                    detailDescription: `Payload object doesn't have '${key}' property!`
-
-                });
-            }
-        }
-
-        let name = req.name;
-        if (typeof name !== 'string') {
-            return prepareFailureMsg({
-                failCode: FailCode.VALIDATION_ERROR,
-                failDescription: "The payload is invalid",
-            }, {
-                detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                detailDescription: "The payload's 'name' property isn't string"
-
-            });
-        }
-
-        let point = req.point;
-        if (typeof point !== 'number') {
-            return prepareFailureMsg({
-                failCode: FailCode.VALIDATION_ERROR,
-                failDescription: "The payload is invalid",
-            }, {
-                detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                detailDescription: "The payload's 'point' property isn't number"
-
+                detailDescription: "Query 'id' property isn't number"
             });
         }
 
         return prepareSuccessMsg({});
     }
 
-    validGetAndDelRequest(name: any): ApiRespData {
-        if (name === null || name === undefined) {
-            return prepareFailureMsg({
-                failCode: FailCode.VALIDATION_ERROR,
-                failDescription: "The query is invalid",
-            }, {
-                detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                detailDescription: "Query doesn't have 'name' property"
-
-            });
-        }
-
-        if (typeof name !== 'string') {
-            return prepareFailureMsg({
-                failCode: FailCode.VALIDATION_ERROR,
-                failDescription: "The query is invalid",
-            }, {
-                detailCode: FailDetailCode.REQUIRED_PROPERTY,
-                detailDescription: "Query 'name' property isn't string"
-
-            });
-        }
-
-        return prepareSuccessMsg({});
+    async makeDefaultLeague(playerName: string, gameName: string): Promise<LeagueData> {
+        let leagues = await this.#dbSvc.selectAllLeagueInfo();
+        let defaultLeague = leagues.sort((element1, element2) => element1.point - element2.point).at(0);
+        return {
+            playerName: playerName,
+            point: 0,
+            matchCount: 0,
+            leagueName: defaultLeague.name,
+            gameName: gameName
+        };
     }
 
-    async add(req: any): Promise<ApiRespData> {
-        let valid = this.validAddAndUpdateRequest(req);
-        if (valid.failure) return valid;
-
-        let leagueInfo: LeagueInfoData = {
-            name: req.name,
-            point: req.point
-        }
-
-        try {
-            let rval = await this.#dbSvc.createLeague(leagueInfo);
-            if (!rval) {
+    async get(query: any) {
+        if (!Object.keys(query).length) {
+            try {
+                return prepareSuccessMsg({ data: await this.#dbSvc.selectAllLeague() });
+            } catch (error) {
                 return prepareFailureMsg({
                     failCode: FailCode.OPERATION_ERROR,
-                    failDescription: `League '${leagueInfo.name}' couldn't be created`,
+                    failDescription: `All leagues couldn't found`,
                     statusCode: 500
                 }, {
-                    detailCode: FailDetailCode.DATABASE_ERROR
+                    detailCode: FailDetailCode.DATABASE_ERROR,
+                    detailDescription: error
+                });
+            }
+        }
+
+        try {
+            if (query.hasOwnProperty("name")) {
+                let rval = await this.#dbSvc.selectLeagueByName(query.name);
+                if (rval == null) {
+                    return prepareFailureMsg({
+                        failCode: FailCode.OPERATION_ERROR,
+                        failDescription: `League name '${query.name}' couldn't found`,
+                    }, {});
+                }
+                return prepareSuccessMsg({ data: rval });
+            } else if (query.hasOwnProperty("game")) {
+                let rval = await this.#dbSvc.selectLeagueByGame(query.game);
+                if (rval == null) {
+                    return prepareFailureMsg({
+                        failCode: FailCode.OPERATION_ERROR,
+                        failDescription: `League game '${query.game}' couldn't found`,
+                    }, {});
+                }
+                return prepareSuccessMsg({ data: rval });
+            } else {
+                return prepareFailureMsg({
+                    failCode: FailCode.VALIDATION_ERROR,
+                    failDescription: "The query is invalid",
+                }, {
+                    detailCode: FailDetailCode.REQUIRED_PROPERTY,
+                    detailDescription: "Query needs to include 'name' or 'game' property"
+
                 });
             }
         } catch (error) {
             return prepareFailureMsg({
                 failCode: FailCode.OPERATION_ERROR,
-                failDescription: `League '${leagueInfo.name}' couldn't be created`,
+                failDescription: `League couldn't found`,
                 statusCode: 500
             }, {
                 detailCode: FailDetailCode.DATABASE_ERROR,
                 detailDescription: error
             });
         }
-
-        return prepareSuccessMsg({ data: `League '${leagueInfo.name}' has created` });
     }
 
-    async get(query: any): Promise<ApiRespData> {
-        if (!Object.keys(query).length) {
-            return prepareSuccessMsg({ data: await this.#dbSvc.selectAllLeague() });
-        }
-
-        let name = query.name;
-        let valid = this.validGetAndDelRequest(name);
-        if (valid.failure) return valid;
-
-        let dbResp: LeagueInfoData;
+    async update(query: any): Promise<ApiRespData> {
         try {
-            dbResp = await this.#dbSvc.selectLeague(name);
-            if (!dbResp) {
+            let fixture: FixtureData[] = [];
+            if (Object.keys(query).length) {
+                let valid = this.validUpdateRequest(query);
+                if (valid.failure) return valid;
+                fixture.push(await this.#dbSvc.selectFixture(parseInt(query.id)));
+            } else {
+                fixture = await this.#dbSvc.selectAllFixture();
+            }
+
+            if (fixture.length == 0) {
                 return prepareFailureMsg({
-                    failCode: FailCode.OPERATION_ERROR,
-                    failDescription: `League '${name}' couldn't found`,
+                    failCode: FailCode.VALIDATION_ERROR,
+                    failDescription: `There is no any fixture`
                 }, {});
             }
-        } catch (error) {
-            return prepareFailureMsg({
-                failCode: FailCode.OPERATION_ERROR,
-                failDescription: `League '${name}' couldn't found`,
-                statusCode: 500
-            }, {
-                detailCode: FailDetailCode.DATABASE_ERROR,
-                detailDescription: error
+
+            let activeMatches: any[] = [];
+            fixture.forEach((element) => {
+                for (const [index, match] of element.matchInfo.entries()) {
+                    if (match.isActive == true) {
+                        activeMatches.push({
+                            info: match,
+                            fixtureId: element.id,
+                            matchIndex: index,
+                            gameName: element.gameName
+                        });
+                    }
+                }
             });
-        }
-
-        return prepareSuccessMsg({ data: dbResp });
-    }
-
-    async update(req: any): Promise<ApiRespData> {
-        let valid = this.validAddAndUpdateRequest(req);
-        if (valid.failure) return valid;
-
-        let leagueInfo: LeagueInfoData = {
-            name: req.name,
-            point: req.point
-        }
-
-        if (!await this.#dbSvc.updateLeague(leagueInfo)) {
-            return prepareFailureMsg({
-                failCode: FailCode.OPERATION_ERROR,
-                failDescription: `League '${leagueInfo.name}' couldn't be updated`,
-                statusCode: 500
-            }, {
-                detailCode: FailDetailCode.DATABASE_ERROR
-            });
-        }
-
-        return prepareSuccessMsg({ data: `League '${leagueInfo.name}' has successfully updated` });
-    }
-
-    async del(name: string): Promise<ApiRespData> {
-        let valid = this.validGetAndDelRequest(name);
-        if (valid.failure) return valid;
-
-        try {
-            let rval = await this.#dbSvc.deleteLeague(name);
-            if (!rval) {
+            if (!activeMatches.length) {
                 return prepareFailureMsg({
                     failCode: FailCode.OPERATION_ERROR,
-                    failDescription: `League '${name}' couldn't be deleted`,
-                }, {
-                    detailDescription: "League couldn't found"
-                });
+                    failDescription: `There is no any active match`,
+                }, {});
             }
+
+            for (const activeMatch of activeMatches) {
+                const match = activeMatch.info;
+                const gameName = activeMatch.gameName;
+
+                // check winner team name
+                let teams = Object.keys(match.teamList);
+                let winningTeam = match.winner;
+                if (!teams.includes(winningTeam)) {
+                    continue;
+                }
+
+                // get winning players
+                let winningPlayerNames = [];
+                for (const team of teams) {
+                    if (team == winningTeam) {
+                        winningPlayerNames.push(...match.teamList[team]);
+                        break;
+                    }
+                }
+                let winningPlayers: LeagueData[] = [];
+                for (const playerName of winningPlayerNames) {
+                    let league = await this.#dbSvc.selectLeagueByName(playerName);
+                    if (league == null) {
+                        winningPlayers.push(await this.makeDefaultLeague(playerName, gameName));
+                    } else {
+                        winningPlayers.push(league);
+                    }
+                }
+
+                // get losing players
+                let losingPlayerNames = [];
+                teams.forEach(element => {
+                    if (element === winningTeam) {
+                        return;
+                    }
+                    losingPlayerNames.push(...match.teamList[element]);
+                });
+                let losingPlayers: LeagueData[] = [];
+                for (const playerName of losingPlayerNames) {
+                    let league = await this.#dbSvc.selectLeagueByName(playerName);
+                    if (league == null) {
+                        losingPlayers.push(await this.makeDefaultLeague(playerName, gameName));
+                    } else {
+                        losingPlayers.push(league);
+                    }
+                }
+
+                // calculate the win points for the winners
+                let winPoint = 0;
+                for (const losingPlayer of losingPlayers) {
+                    let league = await this.#dbSvc.selectLeagueInfo(losingPlayer.leagueName);
+                    if (league != null) {
+                        winPoint += league.point;
+                    }
+                }
+                winPoint = winPoint / losingPlayers.length;
+
+                // increase match count and add winpoints to winning players
+                winningPlayers.map((element) => {
+                    element.matchCount = element.matchCount + 1;
+                    element.point = element.point + winPoint;
+                });
+                // increase match count losing players
+                losingPlayers.map((element) => {
+                    element.matchCount = element.matchCount + 1;
+                });
+
+                // update winning and losing players
+                winningPlayers.forEach(async (element) => {
+                    if (element.matchCount == 1) {
+                        await this.#dbSvc.createLeague(element);
+                    } else {
+                        await this.#dbSvc.updateLeague(element)
+                    }
+                });
+                losingPlayers.forEach(async (element) => {
+                    if (element.matchCount == 1) {
+                        await this.#dbSvc.createLeague(element);
+                    } else {
+                        await this.#dbSvc.updateLeague(element)
+                    }
+                });
+
+                // update match as not active anymore
+                await this.#dbSvc.updateFixtureByActive(activeMatch.fixtureId, activeMatch.matchIndex, false);
+            }
+
+            return prepareSuccessMsg({ data: await this.#dbSvc.selectAllLeague() });
         } catch (error) {
+            let errMsg = error.hasOwnProperty("message") ? error.message : error;
             return prepareFailureMsg({
                 failCode: FailCode.OPERATION_ERROR,
-                failDescription: `League '${name}' couldn't be deleted`,
+                failDescription: `League couldn't be updated`,
                 statusCode: 500
             }, {
                 detailCode: FailDetailCode.DATABASE_ERROR,
-                detailDescription: error
+                detailDescription: errMsg
             });
         }
-
-        return prepareSuccessMsg({ statusCode: 204 });
     }
 }
